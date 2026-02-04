d1c5e8f7376f7b8ab53836d4cd2748f9a7f3f190
hkedge96 <edgargenedeguzman@gmail.com>
2026-02-04 15:53:37 -0700

    updated login credentials

--- test-archive/saveLoginSession.spec.ts
+++ test-archive/saveLoginSession.spec.ts
@@ -6,10 +6,10 @@ test('test', async ({ page }) => {
   await page.goto('https://study-test.byupathway.edu/');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.locator('#signin-container').click();
-  await page.getByRole('textbox', { name: 'Username' }).fill('devtest1@churchofjesuschrist.org');
+  await page.getByRole('textbox', { name: 'Username' }).fill(process.env.EMAIL!);
   await page.getByText('Keep me signed in').click();
   await page.getByRole('button', { name: 'Next' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('byupathway');
+  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.PASSWORD!);
   await page.getByRole('button', { name: 'Verify' }).click();
   await page.getByRole('button', { name: 'Yes' }).click();
   await expect(page.getByRole('heading', { name: 'Download Study Materials' })).toBeVisible();
